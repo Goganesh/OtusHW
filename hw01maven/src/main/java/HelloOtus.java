8c5b68576b66f87222fb23533c101973bfffb270
goganesh <basiladze@mail.ru>
2019-07-03 14:02:56 +0300

    hw-01

---
+++ hw01maven/src/main/java/HelloOtus.java
@@ -0,0 +1,16 @@
+
+import java.util.List;
+import com.google.common.collect.Lists;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        Integer[] array = new Integer[3];
+        array[0] = 10;
+        array[2] = 20;
+        List<Integer> list =  Lists.asList(0, array);
+        for(Integer num : list) {
+            System.out.println(num);
+        }
+
+    }
+}
