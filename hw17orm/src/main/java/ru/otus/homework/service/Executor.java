2ab3cfdaa7ff58c4da58d51e6a38b0af7d8bd324
goganesh <basiladze@mail.ru>
2019-09-17 20:51:57 +0300

    orm homework

--- hw17orm/src/main/java/ru/otus/homework/service/Executor.java
+++ hw17orm/src/main/java/ru/otus/homework/service/Executor.java
@@ -8,7 +8,7 @@ import java.lang.reflect.Modifier;
 
 public class Executor<T> {
     void create(T objectData){
-        checkAnnotation(T);
+        checkAnnotation(objectData.getClass());
     }
     void update(T objectData){
 
